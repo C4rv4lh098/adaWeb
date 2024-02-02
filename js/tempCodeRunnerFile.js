3fbae71a69061c4064c70e6c46e89af290263b85
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2024-02-02 18:12:43 -0400

    End of project

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for (i=0; i<accordion.length; i++){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
---
+++ js/viewFunction.js
@@ -0,0 +1,11 @@
+function accordion() {
+    const accordion = document.getElementsByClassName('box');
+
+    for (i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener('click', function () {
+            this.classList.toggle('active')
+        })
+    }
+
+    console.log("Estou funcionando")
+}
